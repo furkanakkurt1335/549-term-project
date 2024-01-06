5235dc3931466b3f92b82994419c80110248f1f2
Efekan Kavalci <efekankavalci@gmail.com>
2024-01-06 23:36:41 +0300

    initial alignmnets for benchmark (partial)

---
+++ alignment/utils.py
@@ -0,0 +1,90 @@
+import numpy as np
+from Bio.Align import substitution_matrices
+from Bio import Align, SeqIO
+import json, argparse
+
+# Taken from: https://github.com/mgtools/PEbA/blob/master/scripts/utility.py
+def write_msf(seq1: str, seq2: str, id1: str, id2: str, method: str,
+               gopen: float, gext: float, path: str, beg: list, end: list):
+    """Writes alignment to file in msf format
+
+    :param seq1: first aligned sequence
+    :param seq2: second aligned sequence
+    :param id1: first sequence id
+    :param id2: second sequence id
+    :param method: scoring method used
+    :param gopen: gap penalty for opening a new gap
+    :param gext: gap penalty for extending a gap
+    :param path: directory to write alignment to
+    :param list: beginning positions of seqs in alignment
+    :param list: end positions of seqs in alignment
+    """
+
+    fpath = f'{path}/{id1}-{id2}.msf'
+
+    # Add space every 10 characters
+    seq1 = [seq1[i:i+10] for i in range(0, len(seq1), 10)]
+    seq1 = ' '.join(seq1)
+    seq2 = [seq2[i:i+10] for i in range(0, len(seq2), 10)]
+    seq2 = ' '.join(seq2)
+
+    # Split sequences every 50 characters
+    seq1_split = [seq1[i:i+55] for i in range(0, len(seq1), 55)]
+    seq2_split = [seq2[i:i+55] for i in range(0, len(seq2), 55)]
+
+    # Add extra spaces to either id if they are not the same length
+    if len(id1) != len(id2):
+        if len(id1) > len(id2):
+            id2 = id2 + ' ' * (len(id1) - len(id2))
+        else:
+            id1 = id1 + ' ' * (len(id2) - len(id1))
+
+    # Put alignment in string format
+    length = len(seq1)
+    alignment = 'PileUp\n\n\n\n'
+    alignment += f'   MSF: {length}  Type: P  Method: {method}  Gopen: {gopen}  Gext: {gext}\n\n'
+    alignment += f' Name: {id1} oo  Len:  {length}  Start/End:  {beg[0]},{end[0]}\n'
+    alignment += f' Name: {id2} oo  Len:  {length}  Start/End:  {beg[1]},{end[1]}\n\n//\n\n\n\n'
+    for i in range(len(seq1_split)): # pylint: disable=C0200
+        alignment += f'{id1}      {seq1_split[i]}\n'
+        alignment += f'{id2}      {seq2_split[i]}\n\n'
+
+    # If no path is determined then print to console, otherwise write to file
+    if path == 'n':
+        print(alignment)
+    else:
+        with open(fpath, 'w', encoding='utf8') as file:
+            file.write(alignment)
+
+def get_scaled_similarity_matrix(unscaled_matrix_path = "similarity_matrix.json",factor = 10.0):
+    with open(unscaled_matrix_path) as f:
+        our_m = json.load(f)
+    aa_l = ''.join(our_m["aa's"])
+    m = our_m["matrix"]
+
+    blosum62_m = substitution_matrices.load("BLOSUM62") # Load the BLOSUM62 matrix
+    embedding_similarity_matrix = blosum62_m.copy()
+
+    for i in range(len(aa_l)):
+        for j in range(len(aa_l)):
+            try:
+                # Multiply with 10 to bring cosine sims to similar scale with blosum (as done in PEbA paper)
+                embedding_similarity_matrix[aa_l[i]][aa_l[j]] = m[i][j]*factor
+            except:
+                pass
+
+    return embedding_similarity_matrix
+
+
+def align_sequences_biopython(seq1, seq2, penalty_params, matrix, mode = 'global'):
+    aligner = Align.PairwiseAligner()
+    aligner.substitution_matrix = matrix
+    if penalty_params["penalty_type"] == "linear":
+        aligner.open_gap_score = penalty_params["gap_penalty"]
+        aligner.extend_gap_score = penalty_params["gap_penalty"]
+    elif penalty_params["penalty_type"] == "affine":
+        aligner.open_gap_score = penalty_params["gap_penalty"]
+        aligner.extend_gap_score = penalty_params["gap_extension_penalty"]
+
+    aligner.mode = mode
+    return aligner.align(seq1, seq2)
---
+++ alignment/write_alignments.py
@@ -0,0 +1,87 @@
+import argparse, os
+from Bio import Align, SeqIO
+from Bio.Align import substitution_matrices
+import utils
+
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--ref", type=str, default="RV11",help='Name of BALIBASE reference set')
+    parser.add_argument("--alignmode",type=str, default="global", help="Alignment mode, global or local")
+    parser.add_argument("--simscalefactor", type=float,default=10.0)
+    return parser.parse_args()
+
+def align_and_write_msf(seq1,seq2):
+    ...
+
+if __name__ == '__main__':
+    args = parse_arguments()
+    base_data_path = "data/sequences"
+    balibase_ref_path = os.path.join(base_data_path,args.ref)
+    msas = os.listdir(balibase_ref_path)
+
+    # Create the directories
+    blosum62_path = "data/alignments"+"/" + args.alignmode +"_blosum62" 
+    if not os.path.exists(blosum62_path):
+        os.makedirs(blosum62_path)
+    blosum62_ref_path = blosum62_path + "/" + args.ref
+    if not os.path.exists(blosum62_ref_path):
+        os.makedirs(blosum62_ref_path)
+
+    aaembedding_path = "data/alignments"+"/" + args.alignmode +"_aaembedding"
+    if not os.path.exists(aaembedding_path):
+        os.makedirs(aaembedding_path)
+    aaembedding_ref_path = aaembedding_path + "/" + args.ref
+    if not os.path.exists(aaembedding_ref_path):
+        os.makedirs(aaembedding_ref_path)
+
+
+    embedding_similarity_matrix = utils.get_scaled_similarity_matrix(factor=args.simscalefactor)
+    blosum62_m = substitution_matrices.load("BLOSUM62")
+    penalty_params = {"penalty_type": "affine", "gap_penalty": -11, "gap_extension_penalty": -1}
+
+    for msa in msas:
+        print("ON MSA:", msa)
+        msa_path = os.path.join(balibase_ref_path,msa)
+        sequence_list = os.listdir(msa_path)
+
+        msa_alignment_directory_blosum = blosum62_ref_path + "/" + msa
+        if not os.path.exists(msa_alignment_directory_blosum):
+            os.makedirs(msa_alignment_directory_blosum)
+
+        msa_alignment_directory_aaembedding = aaembedding_ref_path + "/" + msa
+        if not os.path.exists(msa_alignment_directory_aaembedding):
+            os.makedirs(msa_alignment_directory_aaembedding)
+
+        for i in range(len(sequence_list)):
+            for j in range(i+1,len(sequence_list)):
+                id1 = sequence_list[i].split('.')[0] 
+                id2 = sequence_list[j].split('.')[0]
+                raw_seq1 = SeqIO.read(os.path.join(msa_path,sequence_list[i]), "fasta")
+                raw_seq2 = SeqIO.read(os.path.join(msa_path,sequence_list[j]), "fasta")
+
+                embedding_alignment = utils.align_sequences_biopython(raw_seq1, raw_seq2, penalty_params, embedding_similarity_matrix,mode=args.alignmode)
+                blosum62_alignment = utils.align_sequences_biopython(raw_seq1, raw_seq2, penalty_params, blosum62_m,mode=args.alignmode)
+                
+                # EMBEDDING BASED ALIGNMENT
+                seq1 = embedding_alignment[0][0]
+                seq2 = embedding_alignment[0][1]
+                if args.alignmode == "global":
+                    # In case of global alignment, beginning indices are 0 and end is the len
+                    beg, end = [0, 0], [len(seq1), len(seq2)]
+                elif args.alignmode == "local":
+                    # TODO: local alignments
+                    ...
+                utils.write_msf(seq1=seq1,seq2=seq2,id1=id1,id2=id2,method = "embeddingmatrix",gopen=-11.0, gext=-1.0,path= msa_alignment_directory_aaembedding, beg=beg, end=end )
+
+                # BLOSUM ALIGNMENT
+                seq1 = blosum62_alignment[0][0]
+                seq2 = blosum62_alignment[0][1]
+                if args.alignmode == "global":
+                    # In case of global alignment, beginning indices are 0 and end is the len
+                    beg, end = [0, 0], [len(seq1), len(seq2)]
+                elif args.alignmode == "local":
+                    # TODO: local alignments
+                    ...
+                utils.write_msf(seq1=seq1,seq2=seq2,id1=id1,id2=id2,method = "blosum62",gopen=-11.0, gext=-1.0,path= msa_alignment_directory_blosum, beg=beg, end=end )
+        break
+    
